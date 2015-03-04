c36b0c188832ba5409fd1bf54e84da1fd55dd131
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-03-03 22:45:48 -0500

    Finish scraper to write TLDs to file.

--- js/TLDScraper.js
+++ js/TLDScraper.js
@@ -4,7 +4,8 @@ const
  , cheerio  = require('cheerio')
  , tldList  = 'http://en.wikipedia.org/wiki/List_of_Internet_top-level_domains';
 
-/* Retrieves the valid top level TLDs and calls back an array. */
+/* Retrieves the valid top level TLDs and inserts them into an array.
+   Calls back the array of TLDs. */
 function getTLDs(callback) {
 
   request(tldList, function(err, response, body) {
@@ -25,3 +26,22 @@ function getTLDs(callback) {
   });
 
 };
+
+/* Write the given content in its original representation to a file. */
+function writeArrayToFile(writeArray) {
+  fs.writeFile('../TLDs.txt',
+    JSON.stringify(writeArray),
+    function(err) {
+      if (err) throw err;
+    }
+  );
+};
+
+/* Get the TLDs and write them to a file. */
+(function main() {
+  getTLDs(function(err, TLDs) {
+    console.log("Retrieving TLDs...");
+    writeArrayToFile(TLDs);
+    console.log("TLDs.txt created successfully.");
+  });
+})();
